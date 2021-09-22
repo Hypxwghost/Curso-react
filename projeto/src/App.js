3c20872e21d09059829faf7fa753ef0980bd7339
Khalli <60857997+Hypxwghost@users.noreply.github.com>
2021-09-21 22:06:45 -0300

    aula 5

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,22 +1,20 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-	const name = 'Khalli'
-
-	function sum(a, b) {
-		return a + b
-	}
-
-  	const url = 'logo192.png'
-
+    const nome = 'Maria'
   	return (
 		<div className="App">
-			<h2>Alterando o JSX</h2>
-			<p>Olá, {name}</p>
-			<p>Soma: {sum(1, 2)}</p>
-			<img src={url} alt="Imagem" />
             <HelloWorld/>
+            <SayMyName nome='Khalli'/>
+            <SayMyName nome={nome}/>
+            <Pessoa nome='Khalli'
+                    idade='19'
+                    profissao='Dev'
+                    foto='foto.jpg'
+            />
 		</div>
   	);
 }
--- projeto/src/components/HelloWorld.jsx
+++ projeto/src/components/HelloWorld.jsx
@@ -3,7 +3,6 @@ import Frase from "./Frase"
 function HelloWorld() {
     return (
         <div>
-            
             <h1>Primeiro Componente</h1>
             <Frase/>
         </div>
