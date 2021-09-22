8844f87a4eb249a5ce2224ef93a507c5354a4ad9
Khalli <60857997+Hypxwghost@users.noreply.github.com>
2021-09-22 20:51:26 -0300

    Aula 7

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import List from './components/List';
 
 function App() {
     const nome = 'Maria'
@@ -15,6 +16,7 @@ function App() {
                     profissao='Dev'
                     foto='foto.jpg'
             />
+            <List/>
 		</div>
   	);
 }
