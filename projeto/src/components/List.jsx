098f4b6ffff273151a4e23b31aad96d883961c10
Khalli <60857997+Hypxwghost@users.noreply.github.com>
2021-09-22 21:04:19 -0300

    aula8

--- projeto/src/components/List.jsx
+++ projeto/src/components/List.jsx
@@ -5,8 +5,10 @@ function List() {
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="Koennigsegg"/>
-                <Item marca="Ford"/>
+                <Item marca="Koennigsegg" ano_lancamento={2000}/>
+                <Item marca="Ford" ano_lancamento={1987}/>
+                <Item marca="Mclaren" ano_lancamento={1999}/>
+                <Item/>
             </ul>
         </>
     )
